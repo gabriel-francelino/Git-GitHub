d3b7f28be9d87a124e5ba08897ec576b05b1d719
Gabriel Francelino <gabriel-francelino>
2023-03-10 15:11:32 -0300

    Alteração no TesteFatorial.java

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,6 +1,6 @@
 public class TesteFatorial {
     public static void main(String[] args) {
         Fatorial fat = new Fatorial();
-        fat.fat(5);
+        System.out.println(fat.fat(5));      
     }
 }
