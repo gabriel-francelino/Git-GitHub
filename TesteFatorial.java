3cc3091427d999b4f206b7a0706961de1b947825
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-03-11 14:29:57 -0300

    Fatorial de 4 em vez de 0

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ public class TesteFatorial {
     // Comentário inserido em TesteFatorial
     public static void main(String[] args) {
         Fatorial fat = new Fatorial();
-        int resultado = fat.fat(0);
+        int resultado = fat.fat(4);
         System.out.println(resultado);      
     }
 }
