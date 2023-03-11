755bd1625280b02e5e1270fbd8dd5c7c83eb8791
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-03-11 11:28:20 -0300

    Exceção para números negativos criada

--- Fatorial.java
+++ Fatorial.java
@@ -1,7 +1,11 @@
 public class Fatorial{
     // Versão iterativa do fatorial
-    public int fat(int n){
+    public int fat(int n) throws IllegalArgumentException{
         int resultado = 1;
+
+        if (n < 0) {
+            throw new IllegalArgumentException("Não existe fatorial para números negativos");
+        }
         
         for(int i = 2; i <= n; i++){
             resultado*= i;
