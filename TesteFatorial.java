4db6b203f5eed7aaecdd5e008ff550cef92dbc77
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-03-11 14:32:34 -0300

    Fatorial de 10 e renomeação da variavel resultadado para 'r'

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ public class TesteFatorial {
     // Comentário inserido em TesteFatorial
     public static void main(String[] args) {
         Fatorial fat = new Fatorial();
-        int resultado = fat.fat(0);
-        System.out.println(resultado);      
+        int r = fat.fat(10);
+        System.out.println(r);      
     }
 }
