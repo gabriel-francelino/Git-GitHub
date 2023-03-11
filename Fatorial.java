3e73f56177af840bc3c41657428f4330869b76c1
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-03-11 13:49:42 -0300

    Comentários em Fatorial.java atualizados

--- Fatorial.java
+++ Fatorial.java
@@ -1,5 +1,5 @@
 public class Fatorial{
-    // Versão iterativa do fatorial
+    // Versão iterativa do fatorial com teste para fatorial de 0
     public int fat(int n) throws IllegalArgumentException{    
         if (n < 0) {
             throw new IllegalArgumentException("Não existe fatorial para números negativos");
